4dbb3314b8750a31822de85f0e17c780b61bb7d9
santiagohuertas5 <111013519+santiagohuertas5@users.noreply.github.com>
2023-03-19 14:27:46 -0500

    .

---
+++ Script HEC.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+require(pacman)
+p_load(tidyverse, rio, skimr, janitor, ggplot2, haven, dplyr, stargazer,broom,writexl) 
+
+
+elec_1958 <- read_dta("1958_Presidencia.dta")
+elec_1962 <- read_dta("1962_presidencia.dta")
+elec_1966 <- read_dta("1966_presidencia.dta")
+elec_1970 <- read_dta("1970_presidencia.dta")
+elec_1974 <- read_dta("1974_presidencia.dta")
+elec_1978 <- read_dta("1978_presidencia.dta")
+elec_1982 <- read_dta("1982_presidencia.dta")
+elec_1986 <- read_dta("1986_presidencia.dta")
+elec_1990 <- read_dta("1990_presidencia.dta")
+elec_1994 <- read_dta("1994_Presidencia_Segunda_Vuelta.dta")
+elec_1998 <- read_dta("1998_Presidencia_Segunda_Vuelta.dta")
+elec_2002 <- read_dta("2002_Presidencia.dta")
+elec_2006 <- read_dta("2002_Presidencia.dta")
+elec_2010 <- read_dta("2010_Presidencia_Primera_Vuelta.dta")
+elec_2014 <- read_dta("2014_Presidencia_Segunda_Vuelta.dta")
+elec_2018 <- read_dta("2018_Presidencia_Primera_Vuelta.dta")
+
+
+
+
+
+
+
+
+
+
--- Untitled.R
+++
@@ -1,9 +0,0 @@
-require(pacman)
-p_load(tidyverse, rio, skimr, janitor, ggplot2, haven, dplyr, stargazer,broom,writexl) 
-
-
-elec_1958 <- read_dta("1958_Presidencia.dta")
-elec_1962 <- read_dta("1962_presidencia.dta")
-elec_1966 <- read_dta("1966_presidencia.dta")
-elec_1970 <- read_dta("1970_presidencia.dta")
-elec_1974 <- read_dta("1974_presidencia.dta")
